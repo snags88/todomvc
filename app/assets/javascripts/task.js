3fbf9bcb576e2a27ceb3f76b3e4d54e160b15643
Seiji Naganuma <Snaganumas@gmail.com>
2015-03-26 22:22:27 -0400

    double click edit feature

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -1,5 +1,8 @@
 $(function(){
   checkboxListener();
+  editListener();
+  blurListener();
+  submitListener();
 });
 
 function checkboxListener(){
@@ -9,4 +12,31 @@ function checkboxListener(){
 function toggleCheckbox(){
   $(this).parents("li").toggleClass("completed");
   $(this).parents("form").trigger("submit");
+}
+
+function editListener(){
+  $("#todo-list").on("dblclick", "label", editHandler);
+}
+
+function editHandler(){
+  $(this).parents("li").toggleClass("editing");
+  $(this).parents("li").find(".edit").focus();
+}
+
+function blurListener(){
+  $("#todo-list").on("blur", ".edit_description", blurHandler);
+}
+
+function blurHandler(){
+  $(this).parents("li").toggleClass("editing");
+}
+
+function submitListener(){
+  $("#todo-list").on("submit", ".edit_description", submitHandler);
+}
+
+function submitHandler(){
+  $(this).find(".edit").blur();
+  var text = $(this).find(".edit").val();
+  $(this).parents("li").find("label").html(text);
 }
